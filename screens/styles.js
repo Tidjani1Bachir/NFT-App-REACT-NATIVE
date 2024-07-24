0a7b0a17c99c03dd2030543457b4ad90fd3c4eca
TIDJANI BACHIR <144597750+Tidjani1Bachir@users.noreply.github.com>
2024-07-24 19:56:26 +0100

    Add files via upload

---
+++ screens/styles.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1, // Allow full height for web or mobile
+    },
+    safeArea: {
+        flex: 1, // Allow full height for mobile
+    },
+    webContent: {
+        flex: 1, // Allow full height for web
+    },
+  });
+
+  export default styles;
